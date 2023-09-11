1162bb8185dc8cb3f56b3cf80288c567747be059
sitjohnny <johnny.sit@uconn.edu>
2023-09-10 21:52:00 -0400

    button enhanced to use different styles

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import backgroundImage from './assets/images/background-image.png';
 import ImageViewer from './src/components/ImageViewer';
+import Button from './src/components/Button';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer placeholderImageSource={backgroundImage} />
-                <Text style={{ color: 'white' }}>Hello World</Text>
-                <StatusBar style="auto" />
             </View>
+            <View style={styles.footerContainer}>
+                <Button theme="primary" label={'Choose a photo'} />
+                <Button label={'Use this photo'} />
+            </View>
+            <StatusBar style="auto" />
         </View>
     );
 }
@@ -26,9 +30,8 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 58,
     },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
     },
 });
