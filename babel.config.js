ee4814fd14558065bbed8fe570325b9d3c82a4d0
sitjohnny <johnny.sit@uconn.edu>
2023-09-10 21:40:27 -0400

    chore: remove unused plugin

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '~': './src',
-          },
-        },
-      ],
-    ],
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+    };
 };
