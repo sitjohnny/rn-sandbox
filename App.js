01a4202490fdb196f7103d543cc3a5747d565d84
sitjohnny <johnny.sit@uconn.edu>
2023-09-10 21:39:56 -0400

    feat: added button and imageviewer components

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import backgroundImage from './assets/images/background-image.png';
+import ImageViewer from './src/components/ImageViewer';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer placeholderImageSource={backgroundImage} />
+                <Text style={{ color: 'white' }}>Hello World</Text>
+                <StatusBar style="auto" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
 });
