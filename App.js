6cc8e5b2484e2459e8145edf3167c04cbed1a1c4
sitjohnny <johnny.sit@uconn.edu>
2023-09-10 22:22:43 -0400

    refactor: tab 4->2, added async image picker

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import backgroundImage from './assets/images/background-image.png';
 import ImageViewer from './src/components/ImageViewer';
 import Button from './src/components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer placeholderImageSource={backgroundImage} />
-            </View>
-            <View style={styles.footerContainer}>
-                <Button theme="primary" label={'Choose a photo'} />
-                <Button label={'Use this photo'} />
-            </View>
-            <StatusBar style="auto" />
-        </View>
-    );
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={backgroundImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label={'Choose a photo'} onPress={pickImageAsync} />
+        <Button label={'Use this photo'} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    imageContainer: {
-        flex: 1,
-        paddingTop: 58,
-    },
-    footerContainer: {
-        flex: 1 / 3,
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
